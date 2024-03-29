ad91c54caa74d1ba82092529840f133c2c45d2be
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-03-29 07:29:34 +0530

    Adding hop plugin. An alternative for Vim Easymotion

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dependencies
+`brew install ripgrep` # This is for live-grep feature in telescope
---
+++ lua/plugins/hop.lua
@@ -0,0 +1,12 @@
+return {
+  'phaazon/hop.nvim',
+  branch = 'v2', -- optional but strongly recommended
+  config = function()
+    -- you can configure Hop the way you like here; see :h hop-config
+    require'hop'.setup{
+			keys = 'etovxqpdygfblzhckisuran'
+		}
+		local hop = require('hop')
+		local directions = require('hop.hint').HintDirection
+  end
+}
