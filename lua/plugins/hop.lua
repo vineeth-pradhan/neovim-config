89ecce99830447284ebe80dead36714ad21bf30b
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-03-29 14:11:09 +0530

    Configuring nvim-tree and hop with shortcuts

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,12 +1,18 @@
 return {
-  'phaazon/hop.nvim',
-  branch = 'v2', -- optional but strongly recommended
-  config = function()
-    -- you can configure Hop the way you like here; see :h hop-config
-    require'hop'.setup{
+	'phaazon/hop.nvim',
+	branch = 'v2', -- optional but strongly recommended
+	config = function()
+		-- you can configure Hop the way you like here; see :h hop-config
+		require'hop'.setup{
 			keys = 'etovxqpdygfblzhckisuran'
 		}
 		local hop = require('hop')
 		local directions = require('hop.hint').HintDirection
-  end
+		vim.keymap.set('n', '<leader>w', function()
+			hop.hint_words({ direction = directions.AFTER_CURSOR })
+		end)
+		vim.keymap.set('n', '<leader>b', function()
+			hop.hint_words({ direction = directions.BEFORE_CURSOR })
+		end)
+	end
 }
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,5 +2,8 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	config = function()
 		require("nvim-tree").setup()
+		local nvim_tree = require("nvim-tree")
+		-- nvim-tree-api.tree.open()
+		vim.keymap.set('n', '<leader>nn', '<cmd>NvimTreeToggle<cr>')
 	end,
 }
